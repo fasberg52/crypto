d1fb82665f1f9b9402d95073934240c79e52a404
mohammadreza.taherian <taherian.1993@gmail.com>
2024-12-30 08:51:01 +0330

    added project crypto

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+// app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UserModule,
+    AdminModule,
+  ],
+})
+export class AppModule {}
